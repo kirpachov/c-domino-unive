68fbc62032c00fc542d805e778d73f1c0cdb8d26
Oleksandr Kirpachov <olecsandr.kirpachov@gmail.com>
2023-10-20 00:18:14 +0200

    NEW user can select any domino (even the invalid ones) and place them to the table

--- DEVELOPMENT.md
+++ DEVELOPMENT.md
@@ -20,3 +20,22 @@ che può essere impostato da configurazione.
 Ciò permetterà:
 - Scrivere codice "commentato", dove i commenti saranno i log stessi
 - Debug semplificato nel momento in cui vi sono presenti bug
+
+## TODO
+- Non permettere all'utente di posizionare domino che non possono essere posizionati
+- Tieni traccia del punteggio e mostralo all'utente sempre.
+- Fai sì che la view sia stabile:
+Quando si posizionano i tasselli dalla mano al tavolo,
+anziché spostare tutto di conseguenza, logga un accapo.
+In questo modo la riga in cui l'utente inserisce i dati non si muove.
+- Feedback: Ad ogni input dell'utente, il sistema deve loggare un feedback.
+Il feedback potrà essere:
+  - Domino `[x|y]` posizionato sul tavolo.
+  - Il domino `[x|y]` non si può posizionare ora.
+  - Messaggio di help nel caso di input non testuali.
+- Funzione ricorsiva dovrà essere in grado di calcolare
+tutte le possibili soluzioni dalla situazione attuale e consigliare l'utente per quella con maggior punteggio.
+  - Si potrebbe aggiungere un * a fianco della mossa consigliata.
+  - Deve essere possibile abilitare questa opzione.
+- Il gioco deve finire se non vi sono mosse ammesse.
+- Mostrare i domino non selezionabili in quel momento in grigetto. Il domino consigliato in verde.
--- main.c
+++ main.c
@@ -2,16 +2,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "log.h"
+// #include "log.c"
 
 /**
  * Max number of dominoes that the user can have.
  */
 #define USER_MAX_DOMINOES_COUNT 100
 #define DOMINOES_UNIVERSE_COUNT 28
+#define USER_COMMAND_MAX_LENGTH 100
+#define COMMAND_HELP "help"
+#define COMMAND_EXIT "exit"
 
 int user_dominoes_count = 5; // TODO make this a input (--dominoes 5) parameter.
 int seed = 0; // TODO make this a input (--seed 1) parameter.
+char last_command[USER_COMMAND_MAX_LENGTH] = "";
+bool is_last_command_invalid = false;
 
 struct Domino {
     int left_value;
@@ -37,11 +44,18 @@ struct Domino table_dominoes[USER_MAX_DOMINOES_COUNT];
 
 int dominoes_on_table = 0;
 
+void remove_user_domino(int index) {
+    for (int i = index; i < user_dominoes_count - 1; i++) {
+        user_dominoes[i] = user_dominoes[i + 1];
+    }
+
+    user_dominoes_count--;
+}
+
 void put_domino_on_table(int index_from_user_dominoes) {
-    printf("TODO put_domino_on_table figa oh");
+    table_dominoes[dominoes_on_table] = user_dominoes[index_from_user_dominoes];
+    remove_user_domino(index_from_user_dominoes);
     dominoes_on_table++;
-    // TODO remove that domino from user's hand
-    // TODO add this domino to table_dominoes
 }
 
 void populate_dominoes_universe() {
@@ -76,46 +90,83 @@ void assign_user_random_dominoes() {
     }
 }
 
-//char* domino_to_string(){
-//    char* domino_string;
-////    char* domino_string = malloc(5 * sizeof(char));
-//    sprintf(domino_string, "[%d|%d]", random_domino().left_value, random_domino().right_value);
-////    sprintf(domino_string, "[%d|%d]", domino.left_value, domino.right_value);
-//    return domino_string;
-//}
-
 void log_table_status() {
     printf("\n");
-    if (dominoes_on_table == 0){
-        printf("<Il tavolo da gioco è vuoto. Butta il primo domino selezionandolo dalla lista. Puoi usare le freccette, WASD, oppure i numeri. Per confermare il domino selezionato, clicca invio.>\n");
-        return;
-    }
+    if (dominoes_on_table == 0) {
+        printf("<Il tavolo da gioco è vuoto.>");
+    } else
+        for (int i = 0; i < dominoes_on_table; i++) {
+            printf("[%d|%d]", table_dominoes[i].left_value, table_dominoes[i].right_value);
+        }
 
-    for(int i = 0; i < dominoes_on_table; i++){
-        printf("[%d|%d]", user_dominoes[i].left_value, user_dominoes[i].right_value);
-    }
-
-    printf("\n");
+    printf("\n\n");
 }
 
-/**
- * User can select dominoes with arrows.
- * So we need to store the current indes he's selecting.
- */
-int current_domino_selection = 0;
+void log_user_dominoes() {
+    printf("Seleziona il domino che vuoi mettere sul tavolo, e conferma con <invio>\n");
 
-void log_user_dominoes(){
-    printf("\n");
-
-    for(int i = 0; i < user_dominoes_count; i++){
+    for (int i = 0; i < user_dominoes_count; i++) {
         printf("%i) [%d|%d]\n", i, user_dominoes[i].left_value, user_dominoes[i].right_value);
     }
 }
 
+
+bool is_last_command(const char *command) {
+    return strcmp(last_command, command) == 0;
+}
+
+int potenza(const int initial, const int elevate_at) {
+    if (elevate_at <= 0) return 1;
+
+    int final = initial;
+    for (int i = 0; i < elevate_at - 1; i++) final *= initial;
+    return final;
+}
+
+bool str_equals_int(const char *str, const int num, const int str_length) {
+    int str_int = 0;
+    for (int i = 0; i < str_length; i++) {
+        const int current = (((char) str[i]) - 48) * potenza(10, (str_length - 1) - i);
+        str_int += current;
+    }
+
+    return str_int == num;
+}
+
+
+void exec_command() {
+    log_debug("exec_command: %s\n", last_command);
+    for (int i = 0; i < user_dominoes_count; i++) {
+        const char command_index[2] = {i, '\0'};
+
+        const bool user_is_selecting_this_domino = str_equals_int(last_command, i, 1);
+
+        if (user_is_selecting_this_domino) {
+            log_debug("Putting down domino at index %d", i);
+            put_domino_on_table(i);
+            return;
+        } else
+            log_debug("Skipping domino at index %d ; Requested is %s", i, command_index);
+    }
+}
+
+
+void log_last_command_status() {
+    if (is_last_command("") || is_last_command(COMMAND_HELP) || is_last_command("?")) {
+        printf("Seleziona le tessere disponibili scrivendo il loro indice e poi clicca invio\n");
+    }
+}
+
 void log_status() {
+    printf("\e[1;1H\e[2J");
+//    printf("CLEARED\n");
+
     log_table_status();
     log_user_dominoes();
     printf("\n");
+
+    // if (!is_last_command("")) printf("$ %s\n", last_command);
+    log_last_command_status();
 }
 
 /**
@@ -130,6 +181,24 @@ void initialize() {
 
 void start_game() {
     log_status();
+    while ((!is_last_command(COMMAND_EXIT)) && user_dominoes_count > 0) {
+        printf("$ ");
+        if (scanf("%s", last_command) != 1) {
+            log_error("scanf returned an error.");
+            continue;
+        }
+
+        exec_command();
+        log_status();
+    }
+
+    if (user_dominoes_count == 0) {
+        log_info("Game has ended because user has no more dominoes.");
+    }
+
+    if (is_last_command(COMMAND_EXIT)) {
+        log_info("Game has ended because user has typed exit.");
+    }
 }
 
 /**
